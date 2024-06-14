38b94e7992cdfe86c2b9d17b32a6926aa4622b80
Redsskull <ivry91@gmail.com>
2024-06-14 11:17:10 +0200

    add basic blog function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,17 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const posts = ['myfirstpost.md', 'mysecondpost.md']; // Add the names of your posts here
+
+posts.forEach(post => {
+  fetch(`posts/${post}`)
+    .then(response => response.text())
+    .then(text => {
+      const html = marked(text);
+      const article = document.createElement('article');
+      article.innerHTML = html;
+      document.querySelector('main').appendChild(article);
+    });
+});
+

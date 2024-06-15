ce3ba740b374c012ef5be39f88010cf9c652bdc0
Redsskull <ivry91@gmail.com>
2024-06-15 07:16:53 +0200

    started marked libarary for blog conversion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () =>{
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
 
-// const posts = ['myfirstpost.md', 'mysecondpost.md']; // Add the names of your posts here
-//
-// posts.forEach(post => {
-//   fetch(`posts/${post}`)
-//     .then(response => response.text())
-//     .then(text => {
-//       const html = marked(text);
-//       const article = document.createElement('article');
-//       article.innerHTML = html;
-//       document.querySelector('main').appendChild(article);
-//     });
-// });
-//
+//take my markdown file and convert it to html
+window.onload = function () {
+  const posts = ["test.md"]; // Add the names of your posts here
+
+posts.forEach((post) => {
+  fetch(`assets/posts/${post}`)
+    .then((response) => response.text())
+    .then((text) => {
+      const html = window.marked.marked(text);
+      const postDiv = document.createElement("div");
+      postDiv.className = "post";
+      postDiv.innerHTML = html;
+      document.querySelector(".posts").appendChild(postDiv);
+    });
+});
+
+};
---
+++ assets/posts/test.md
@@ -0,0 +1,20 @@
+# This is a test
+
+<!-- write test text here-->
+
+```python
+print("Hello World")
+```
+
+```python
+print("Hello World")
+```
+
+```python
+print("Hello World")
+```
+
+```python
+
+```
+
